2f45573fd2011d3d462bb7c820515e14d5ef0efa
liuss <lssemail@126.com>
2019-01-05 18:34:47 +0800

    add test case

---
+++ src/com/liusir/thread/basic/TestCountDownLatch.java
@@ -0,0 +1,86 @@
+package com.liusir.thread.basic;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Administrator on 2019-1-5.
+ */
+public class TestCountDownLatch {
+
+    private static int NUM = 10;
+    //https://www.cnblogs.com/lizhangyong/p/8906774.html
+
+    public static void main(String[] args) {
+
+        VideoConference conference = new VideoConference(NUM);
+        new Thread(conference).start();
+        for (int i = 0; i< NUM - 1; i++){
+            Participant participant = new Participant(conference, "Participant " + i);
+            new Thread(participant).start();
+        }
+
+
+    }
+}
+
+class VideoConference implements Runnable{
+
+    private final CountDownLatch latch;
+
+    public VideoConference(int n){
+
+        latch = new CountDownLatch(n);
+    }
+
+    public void arrive(String name){
+
+        System.out.printf("%s has arrived.\n", name);
+        latch.countDown();
+        System.out.printf("waiting count for %s arrived.\n", String.valueOf(latch.getCount()));
+    }
+
+    @Override
+    public void run() {
+
+        synchronized (VideoConference.class){
+            if(latch.getCount() != 0){
+                System.out.printf("VideoConference: Initialization: %d participants.\n", latch.getCount());
+            }
+        }
+
+        try {
+            latch.await();
+            System.out.printf("VideoConference: All the participants have come\n");
+            System.out.printf("VideoConference: Let's start...\n");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Participant implements Runnable{
+
+    private VideoConference conference;
+    private String name;
+
+    public Participant(VideoConference conference, String name){
+
+        this.conference = conference;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+
+        long during = (long) Math.random() * 1000;
+
+        try {
+            TimeUnit.SECONDS.sleep(during);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        conference.arrive(name);
+
+    }
+}
---
+++ src/com/liusir/thread/basic/TestCyclicBarrier.java
@@ -0,0 +1,54 @@
+package com.liusir.thread.basic;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Administrator on 2019-1-5.
+ */
+public class TestCyclicBarrier {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(num,()->{
+            System.out.println("start video...");
+        });
+        for (int i = 0; i< num; i++){
+            new Thread(new MyParticipant(cyclicBarrier, "Participant " + i)).start();
+        }
+        System.out.println("end");
+    }
+}
+
+class MyParticipant implements Runnable{
+
+
+    private CyclicBarrier barrier;
+    private String name;
+
+    public MyParticipant(CyclicBarrier barrier, String name){
+
+        this.barrier = barrier;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+
+        long during = (long) Math.random() * 100;
+        try {
+            TimeUnit.SECONDS.sleep(during);
+            System.out.printf("%s is coming\n", name);
+            barrier.await();
+            System.out.printf("%s is watching video\n", name);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch (BrokenBarrierException e){
+
+        }
+
+
+    }
+}
