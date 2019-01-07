3ddf952284f06850a2707dd5336b6f0c56385abb
liuss <lssemial@126.com>
2019-01-07 22:43:39 +0800

    future, callable

---
+++ src/com/liusir/thread/basic/BlockingQueueTest.java
@@ -0,0 +1,112 @@
+package com.liusir.thread.basic;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueueTest {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("input base directory");
+        String directory = in.nextLine();
+        System.out.print("key word:");
+        String keyword = in.nextLine();
+
+        final int FILE_QUEUE_SIZE = 10;
+        final int SEARCH_THREADS = 100;
+        BlockingQueue<File> queue = new ArrayBlockingQueue<>(FILE_QUEUE_SIZE);
+        new Thread(new FileEnumerationTask(queue, new File(directory))).start();
+        for (int i=0; i< SEARCH_THREADS; i++){
+            new Thread(new SearchTask(queue, keyword)).start();
+        }
+
+
+    }
+}
+class FileEnumerationTask implements Runnable{
+
+    public static File DUMMY = new File("");
+    private BlockingQueue<File> blockingQueue;
+    private File start;
+
+    public FileEnumerationTask(BlockingQueue queue, File start){
+
+        this.blockingQueue = queue;
+        this.start = start;
+
+    }
+
+    public void enumerate(File directory){
+
+        File[] files = directory.listFiles();
+        for (File file: files){
+            if(file.isDirectory()){
+                enumerate(file);
+            }else {
+                blockingQueue.add(file);
+            }
+        }
+
+    }
+
+    @Override
+    public void run() {
+
+        enumerate(start);
+        blockingQueue.add(DUMMY);
+    }
+}
+
+class SearchTask implements Runnable{
+
+    private BlockingQueue<File> blockingQueue;
+    private String keyword;
+
+    public SearchTask(BlockingQueue queue, String keyword){
+
+        this.blockingQueue = queue;
+        this.keyword = keyword;
+
+    }
+
+    public void search(File file) throws IOException {
+
+        Scanner in = new Scanner(new FileInputStream(file));
+        int lineNumber = 0;
+        while (in.hasNextLine()){
+            lineNumber++;
+            String line = in.nextLine();
+            if(line.contains(keyword)){
+                System.out.printf("%s:%d:%s%n", file.getPath(), lineNumber, line);
+            }
+        }
+        in.close();
+
+    }
+
+
+    @Override
+    public void run() {
+
+        try {
+            boolean done = false;
+            while (!done){
+                File file = blockingQueue.take();
+                if(file == FileEnumerationTask.DUMMY){
+                    blockingQueue.put(file);
+                    done = true;
+                }else {
+                    search(file);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch (IOException e){
+
+        }
+    }
+}
---
+++ src/com/liusir/thread/basic/FutureTest.java
@@ -0,0 +1,97 @@
+package com.liusir.thread.basic;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+public class FutureTest {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("input base directory");
+        String directory = in.nextLine();
+        System.out.print("key word:");
+        String keyword = in.nextLine();
+
+        MatchCounter counter = new MatchCounter(new File(directory), keyword);
+        FutureTask<Integer> task = new FutureTask<>(counter);
+        new Thread(task).start();
+
+        try {
+            System.out.println(task.get() + "matching files");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
+class MatchCounter implements Callable<Integer>{
+
+    private File directory;
+    private String keyword;
+    private int count;
+
+
+    public MatchCounter(File directory, String keyword){
+
+        this.directory = directory;
+        this.keyword = keyword;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+
+        count = 0;
+        File[] files = directory.listFiles();
+        ArrayList<Future<Integer>> results = new ArrayList<>();
+        for (File file : files){
+            if (file.isDirectory()){
+                MatchCounter counter = new MatchCounter(file, keyword);
+                FutureTask<Integer> task = new FutureTask<>(counter);
+                results.add(task);
+                Thread thread = new Thread(task);
+                thread.start();
+            }else {
+                if(search(file)){
+                    count++;
+                }
+            }
+        }
+
+        for (Future<Integer> result : results){
+            count += result.get();
+
+        }
+        return count;
+    }
+
+    public boolean search(File file){
+        try {
+            Scanner in = new Scanner(new FileInputStream(file));
+            boolean found = false;
+            while (!found && in.hasNextLine()){
+                String line = in.nextLine();
+                if(line.contains(keyword)){
+                    found = true;
+                }
+            }
+            in.close();
+            return found;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
+
